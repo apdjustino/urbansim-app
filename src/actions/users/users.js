b7538aef2284f04a22931078bb6944b1231cfc57
Justin Martinez <justinmartinez14@gmail.com>
2017-12-29 17:09:51 -0700

    included password requirments

--- src/actions/users/users.js
+++ src/actions/users/users.js
@@ -269,6 +269,7 @@ export const submitPasswordReset = (email) => {
 export const resetPassword = (token, password1, password2) => {
     return (dispatch) => {
         let data = {};
+        const regex = new RegExp("^([a-zA-Z0-9@*#]{8,25})$")
         if(password1 == password2){
             data = {token: token, password: password1}
         }else{
@@ -279,6 +280,14 @@ export const resetPassword = (token, password1, password2) => {
                 }
             }));
             return;
+        }
+        if(!regex.test(password1)){
+            dispatch(resetPasswordFail({
+                response: {
+                    status: 403,
+                    statusText: "Password must consists of at least 8 characters and not more than 15 characters and not use any special characters."
+                }
+            }));
         }
         return makeUserRequest("post", data, "/api/passwordreset")
             .then(response => {
